fd58e82a57c7182ad9da4a8bdcfe136e6e9a8019
yeji0812 <yj_0812@naver.com>
2024-07-24 11:49:14 +0900

    수정

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import random
 
-st.header('주사위 게임',diveder = 'rainbow')
-st.button('주사위 던지기',type = 'primary')
+st.header('주사위 게임', divider='rainbow')
+clicked = st.button('주사위 던지기', type='primary')
 
+if clicked:
+    n = random.randint(1,6)
+    st.image(f'./img/{n}.png')
